73abdb121c3de5eb2b6ac53616c3bd68da654a4a
luodahui <934803813@qq.com>
2020-03-23 14:04:51 +0800

    update

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,10 +21,10 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
+  title: 'Test Docusaurus Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://luo-dahui.github.io/', // Your website URL
+  baseUrl: '/test_Docusaurus/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
